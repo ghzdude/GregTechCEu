49f4503ea267cb5244df36197f0a8b687e4249cc
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-11-16 20:16:14 -0700

    use combined inventory for type value

--- src/main/java/gregtech/common/metatileentities/storage/MetaTileEntityQuantumChest.java
+++ src/main/java/gregtech/common/metatileentities/storage/MetaTileEntityQuantumChest.java
@@ -549,7 +549,7 @@ public class MetaTileEntityQuantumChest extends MetaTileEntityQuantumStorage<IIt
 
     @Override
     public IItemHandler getTypeValue() {
-        return this.itemInventory;
+        return this.combinedInventory;
     }
 
     @Override
